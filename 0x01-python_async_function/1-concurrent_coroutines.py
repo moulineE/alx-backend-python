a98594f1939b2c4cc235be0496b816986e060a9f
moulineE <elmahdi.mouline@live.fr>
2024-01-16 01:15:23 +0100

    Task 1. Let's execute multiple coroutines at the same time with async -fix req

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,7 +3,6 @@
 async routine called wait_n that takes in 2 int takes in 2 int arguments
 n and max_delay and returns the list of all the delays (float values)
 """
-import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
