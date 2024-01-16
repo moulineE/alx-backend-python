c9f37b31448d15e4e68cae0a2485791eff0f146f
moulineE <elmahdi.mouline@live.fr>
2024-01-16 01:24:01 +0100

    Task 1. Let's execute multiple coroutines at the same time with async -fix type error

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Args:
         n (int): number of iterations
